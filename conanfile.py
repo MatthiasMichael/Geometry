040e0bd8b7a483feb0f688b1115b118a9ed98c28
MatthiasMichael <matthias.michael@rub.de>
2019-05-22 01:22:32 +0200

    Simplified building examples. Switched to TARGETS cmake generator.

--- conanfile.py
+++ conanfile.py
@@ -7,12 +7,50 @@ class GeometryConan(ConanFile):
     generators = "cmake"
     exports_sources = "cmake/*", "include/*", "src/*"
     requires = "NamedType/1.0@MatthiasMichael/stable", "eigen/3.3.7@conan/stable", "boost_qvm/1.66.0@bincrafters/stable"
+    options = {
+        "build_test_convex_hull": [True, False],
+        "build_test_coordinate_transform": [True, False],
+        "build_test_operations": [True, False],
+        "build_test_projective_transform": [True, False],
+        "build_all_tests": [True, False]
+    }
+    default_options = {
+        "build_test_convex_hull": False,
+        "build_test_coordinate_transform": False,
+        "build_test_operations": False,
+        "build_test_projective_transform": False,
+        "build_all_tests": False
+    }
+
+    def requirements(self):
+        if self.options.build_test_coordinate_transform or self.options.build_all_tests:
+            self.requires("OsgVisualization/1.0@MatthiasMichael/stable")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_EXAMPLES"] = \
+            self.options.build_test_convex_hull or \
+            self.options.build_test_coordinate_transform or \
+            self.options.build_test_operations or \
+            self.options.build_test_projective_transform or \
+            self.options.build_all_tests
+        
+        cmake.definitions["BUILD_EXAMPLE_CONVEX_HULL"] = self.options.build_test_convex_hull or self.options.build_all_tests
+        cmake.definitions["BUILD_EXAMPLE_COORDINATE_TRANSFORM"] = self.options.build_test_coordinate_transform or self.options.build_all_tests
+        cmake.definitions["BUILD_EXAMPLE_OPERATIONS"] = self.options.build_test_operations or self.options.build_all_tests
+        cmake.definitions["BUILD_EXAMPLE_PROJECTIVE_TRANSFORM"] = self.options.build_test_projective_transform or self.options.build_all_tests
+        
+        cmake.configure(source_dir="%s/cmake" % self.source_folder)
+        
+        return cmake
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_EXAMPLES"] = False
-        cmake.configure(source_dir="%s/cmake" % self.source_folder)
+        cmake = self._configure_cmake()
         cmake.build()
+
+    def imports(self):
+        if self.options.build_test_coordinate_transform or self.options.build_all_tests:
+            self.copy("*.dll", "test/test_coordinate_transform", "bin")
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
